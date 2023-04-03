5215dbac470d9064d763bb4168eeacd6700c3968
Саша <Akelas19>
2023-04-03 20:34:12 +0700

    Initial Commit

---
+++ UniversityMap/UniversityMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityMapApp.swift
+//  UniversityMap
+//
+//  Created by Александр Переславцев on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct UniversityMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
